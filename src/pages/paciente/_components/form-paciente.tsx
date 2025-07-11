eeaa438b71d0353c512abebe2db9178e232b4768
ASO <Alvarito1624@gmail.com>
2025-07-10 23:50:24 -0400

    impotaciones inesesarias

--- src/pages/paciente/_components/form-paciente.tsx
+++ src/pages/paciente/_components/form-paciente.tsx
@@ -11,7 +11,7 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { PacienteBaseSchema, PacienteSchema, type Paciente } from "@/types/paciente"
+import { PacienteSchema, type Paciente } from "@/types/paciente"
 import { CrearPaciente, Update } from "@/services/paciente-service"
 
 interface Props{
